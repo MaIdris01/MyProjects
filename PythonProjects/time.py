b66948afc4b29c91948fff07f98eb7b9a819ec01
MAIdris01 <maidris2001@gmail.com>
2022-07-14 07:43:55 +0100

    Time

---
+++ PythonProjects/time.py
@@ -0,0 +1,13 @@
+
+x = "morning"
+y = "afternoon"
+z = "night"
+time =  input("what is the time?\n")
+if (time == x):
+    print("Good Morning")
+elif (time == y):
+    print("Good Afternoon")
+elif (time == z):
+    print("Good Night")
+else:
+    print("Tell me if it's morning, afternoon or night. Thank You!")
